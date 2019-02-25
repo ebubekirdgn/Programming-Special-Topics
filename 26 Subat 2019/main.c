1e64c9830e557d5bfd3a7ffd9341a976a7f6f308
Ebubekir Doğan <ebubekir.dogan@bil.omu.edu.tr>
2019-02-25 10:36:20 +0200

    Update main.c

--- 26 Subat 2019/main.c
+++ 26 Subat 2019/main.c
@@ -13,25 +13,25 @@ void print_array(int *m,int uzunluk)
 	
 }
 int main(int argc) {
-	int m1[] = {4,1};
-	int m2[] = {4,2};
+	int m1[] = {3,5};
+	int m2 [] = {1,2};
+	int m3[] = {{1},{2}};
 	int outMatrix[] = {0,0};
 	
 	printf("Toplam : ");
 	matrix_add(outMatrix,m1,m2,1,1);
-	print_array(outMatrix,4);
+	print_array(outMatrix,1);
 	printf("\n");
 	
 	printf("Carpim : ");
-	matrix_mul(outMatrix,m1,m2,1,1,1);
-	print_array(outMatrix,4);
+	matrix_mul(outMatrix,m1,m3,1,2,1);
+	print_array(outMatrix,1);
 	printf("\n");
 	 
 	printf("Cikarma : ");
 	matrix_sub(outMatrix,m1,m2,1,1);
-	print_array(outMatrix,4);
+	print_array(outMatrix,1);
 	printf("\n");
 	
 	return 0;
 }
-
